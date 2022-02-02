044f0d81b661155cfa42aa720444ed87af33366d
bbaiser <benj.baiser@gmail.com>
2022-02-02 12:48:06 -0500

    fixing names to match found some errors in sp column of the imported data

--- Code/pre_analysis.R
+++ Code/pre_analysis.R
@@ -24,18 +24,60 @@ sp_list<-subset(full_sp, !duplicated(sp))%>%
          dplyr::select(sp, native_status,introduced_status,invasive_status, cultivated_status,status)%>%
          left_join( .,fleppc, by = "sp")#join fleppc list by species name
 
-#How many invasive species are in the data set
+
+#compare flepcc names to those in the data set
+#filter out all duplicate rows (i.e., both pairs of the duplicate) from the svl data
+unshared_names<-fleppc[!fleppc$sp %in% sp_list$sp,]
+
+#diff names
+
+#sp list                            #fleppc
+"Bauhinia variegata var. variegata" == "Bauhinia variegata"# drop var....
+"Pennisetum purpureum" == "Cenchrus purpureus (Pennisetum purpureum)" #drop Cenchrus purpureus
+"Dolichandra unguis-cat" == "Dolichandra unguis-cati (Macfadyena unguis-cati)"#drop macadyena....
+"Lantana camara" == "Lantana strigocamara" #change to strigocamara
+"Schinus terebinthifolius" == "Schinus terebinthifolia"#replace folius with folia
+
+"Sporobolus pyramidalis"	"Sporobolus jacquemontii" #which is which/ask jenn
+
+"Triadica sebifera" =="Triadica sebifera (Sapium sebiferum)"# drop (Sapium sebiferum)
+
+"Aristolochia elegans"== "Aristolochia elegans (Aristolochia littoralis)" #drop(Aristolochia littoralis)
+
+"Pennisetum polystachion"== "	Cenchrus polystachios (Pennisetum polystachios)"#change both to Cenchrus polystachios
+"Pennisetum setaceum"== "Cenchrus setaceus (Pennisetum setaceum)" #change both to Cenchrus setaceus 
+
+"Dalechampia scandens" == "Dalechampia scandens*" # remove asterisk
+
+"Merremia tuberosa" =="Distimake tuberosus (Merremia tuberosa)"# change Distimake tuberosus(Merremia tuberosa) to Merremia tuberosa) to
+"Sansevieria hyacinthoides" == "Dracaena hyacinthoides (Sansevieria hyacinthoides)"# change Dracaena hyacinthoides (Sansevieria hyacinthoides to Sansevieria hyacinthoides
+
+"Epipremnum pinnatum cv. Aureum" == "Epipremnum pinnatum cv.‘Aureum’"# remove quotations from fleppc
+
+"Heteropterys brachiata"=="Heteropterys brachtiata" # fleppc mispelled
+
+"Koelreuteria elegans subsp. formosana"== 	" Koelreuteria elegans subsp. Formosana" #change capitl for F
+"Melaleuca viminalis"== "Melaleuca viminalis (Callistemon viminalis)" # DROP (Callistemon viminalis)
+"Hibiscus tiliaceus var. tiliaceus"       ==      "Talipariti tiliaceum"# Change to Hibiscus tiliaceus var. tiliaceus
+
+"Panicum maximum" =="Urochloa maxima (Panicum maximum)"# change to Panicum maximum
+  
+  
+  
+  #How many invasive species are in the data set?
 int<-sp_list%>%
      filter(Category =="I"| Category =='II')%>%
      count(.)    
-1
-int #122 
 
-sp_list[sp_list$Category == "NA" & sp_list$native_status == "Native", "NA"] <- "Native"
+int #122 invasive species 
+
+#sp_list[sp_list$Category == "NA" & sp_list$native_status == "Native", "NA"] <- "Native"
 
 new<-sp_list %>%
     mutate(Category = ifelse(native_status == 'Native',"Native",Category))%>%
-    #not workingmutate(Category = ifelse(native_status == 'Not Native',"Not Native",Category))
+    mutate_at(vars(Category), ~replace_na(., "Not Native"))
+    
+    mutate(Category = ifelse(native_status == 'Not Native' & Category!="I"| Category !='II',"Not Native",Category))
   
 str(new)
 
