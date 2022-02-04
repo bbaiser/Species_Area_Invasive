2a2df56be8df181900ea2ebb3eea6957701e54c4
bbaiser <benj.baiser@gmail.com>
2022-02-04 11:31:27 -0500

    matched names between flepcc and sp_list created cleaned data set with speices ocurence park name and native invasive designation

--- Code/pre_analysis.R
+++ Code/pre_analysis.R
@@ -1,22 +1,50 @@
-library(tidyverse)
 
+rm(list=ls())
 #isa: had to install rcpp before
 library(sf)
 library(lubridate)
-library(plyr) #You have loaded plyr after dplyr - this is likely to cause problems.If you need functions from both plyr and dplyr, please load plyr first, then dplyr:
+library(plyr)
+library(dplyr)
+ #You have loaded plyr after dplyr - this is likely to cause problems.If you need functions from both plyr and dplyr, please load plyr first, then dplyr:
 library(Ostats)
 library(ggplot2)
 library(iNEXT)
 library(piecewiseSEM)
 library(effects)
 library(raster)
-
+library(tidyverse)
 
 #read in the Full species list for all parks in long format
-full_sp<-read.csv("Data/sp_list_parks_cleaned.csv")
+full_sp<-read.csv("Data/sp_list_parks_cleaned.csv")%>%
+         mutate(sp = replace(sp, sp == "Pennisetum polystachios", "Cenchrus polystachios"))%>%
+         mutate(sp = replace(sp, sp == "Pennisetum setaceum", "Cenchrus setaceus"))%>%
+         mutate(sp = replace(sp, sp == "Bauhinia variegata var. variegata", "Bauhinia variegata"))%>%
+         mutate(sp = replace(sp, sp == "Schinus terebinthifolius", "Schinus terebinthifolia"))%>%
+         mutate(sp = replace(sp, sp == "Pennisetum polystachion", "Cenchrus polystachios"))%>%
+         mutate(sp = replace(sp, sp == "Pennisetum setaceum", "Cenchrus setaceus"))
 
+  
 fleppc<-read.csv("Data/FLEPPC_2019.csv")%>%
-        rename(., sp = Scientific.Name)#rename the species column to match the full_sp
+        dplyr::rename( sp = Scientific.Name)%>%#rename the species column to match the full_sp
+        mutate(sp = replace(sp, sp == "Cenchrus purpureus (Pennisetum purpureum)", "Pennisetum purpureum"))%>%#harmonize names in fleppc list; differences 
+        mutate(sp = replace(sp, sp == "Dolichandra unguis-cati (Macfadyena unguis-cati)", "Dolichandra unguis-cati"))%>%
+        mutate(sp = replace(sp, sp == "Triadica sebifera (Sapium sebiferum)", "Triadica sebifera"))%>%
+        mutate(sp = replace(sp, sp == "Aristolochia elegans (Aristolochia littoralis)", "Aristolochia elegans"))%>%
+        mutate(sp = replace(sp, sp == "Cenchrus polystachios (Pennisetum polystachios)", "Cenchrus polystachios"))%>%
+        mutate(sp = replace(sp, sp == "Cenchrus setaceus (Pennisetum setaceum)", "Cenchrus setaceus"))%>%
+        mutate(sp = replace(sp, sp == "Dalechampia scandens*", "Dalechampia scandens"))%>%
+        mutate(sp = replace(sp, sp == "Distimake tuberosus (Merremia tuberosa)", "Merremia tuberosa"))%>%
+        mutate(sp = replace(sp, sp == "Dracaena hyacinthoides (Sansevieria hyacinthoides)", "Sansevieria hyacinthoides"))%>%
+        mutate(sp = replace(sp, sp == "Epipremnum pinnatum cv.‘Aureum’", "Epipremnum pinnatum cv. Aureum"))%>%
+        mutate(sp = replace(sp, sp == "Heteropterys brachtiata", "Heteropterys brachiata"))%>%
+        mutate(sp = replace(sp, sp == "Koelreuteria elegans subsp. Formosana", "Koelreuteria elegans subsp. formosana"))%>%  
+        mutate(sp = replace(sp, sp == "Melaleuca viminalis (Callistemon viminalis)", "Melaleuca viminalis "))%>% 
+        mutate(sp = replace(sp, sp == "Talipariti tiliaceum", "Hibiscus tiliaceus var. tiliaceus"))%>% 
+        mutate(sp = replace(sp, sp == "Urochloa maxima (Panicum maximum)", "Panicum maximum"))
+  
+
+#####"Sporobolus pyramidalis"	"Sporobolus jacquemontii" #which is which/ask jenn
+
 
 #Obtain a list of all species (i.e., no duplicates form the full species list along with status)
 
@@ -24,97 +52,22 @@ sp_list<-subset(full_sp, !duplicated(sp))%>%
          dplyr::select(sp, native_status,introduced_status,invasive_status, cultivated_status,status)%>%
          left_join( .,fleppc, by = "sp")#join fleppc list by species name
 
-
-#compare flepcc names to those in the data set
-#filter out all duplicate rows (i.e., both pairs of the duplicate) from the svl data
+#compare flepcc names to those in the data set and find fleppc species not in the species list
 unshared_names<-fleppc[!fleppc$sp %in% sp_list$sp,]
 
-#diff names
 
-#sp list                            #fleppc
-"Bauhinia variegata var. variegata" == "Bauhinia variegata"# drop var....
-"Pennisetum purpureum" == "Cenchrus purpureus (Pennisetum purpureum)" #drop Cenchrus purpureus
-"Dolichandra unguis-cat" == "Dolichandra unguis-cati (Macfadyena unguis-cati)"#drop macadyena....
-"Lantana camara" == "Lantana strigocamara" #change to strigocamara
-"Schinus terebinthifolius" == "Schinus terebinthifolia"#replace folius with folia
-
-"Sporobolus pyramidalis"	"Sporobolus jacquemontii" #which is which/ask jenn
-
-"Triadica sebifera" =="Triadica sebifera (Sapium sebiferum)"# drop (Sapium sebiferum)
-
-"Aristolochia elegans"== "Aristolochia elegans (Aristolochia littoralis)" #drop(Aristolochia littoralis)
-
-"Pennisetum polystachion"== "	Cenchrus polystachios (Pennisetum polystachios)"#change both to Cenchrus polystachios
-"Pennisetum setaceum"== "Cenchrus setaceus (Pennisetum setaceum)" #change both to Cenchrus setaceus 
-
-"Dalechampia scandens" == "Dalechampia scandens*" # remove asterisk
-
-"Merremia tuberosa" =="Distimake tuberosus (Merremia tuberosa)"# change Distimake tuberosus(Merremia tuberosa) to Merremia tuberosa) to
-"Sansevieria hyacinthoides" == "Dracaena hyacinthoides (Sansevieria hyacinthoides)"# change Dracaena hyacinthoides (Sansevieria hyacinthoides to Sansevieria hyacinthoides
-
-"Epipremnum pinnatum cv. Aureum" == "Epipremnum pinnatum cv.‘Aureum’"# remove quotations from fleppc
-
-"Heteropterys brachiata"=="Heteropterys brachtiata" # fleppc mispelled
-
-"Koelreuteria elegans subsp. formosana"== 	" Koelreuteria elegans subsp. Formosana" #change capitl for F
-"Melaleuca viminalis"== "Melaleuca viminalis (Callistemon viminalis)" # DROP (Callistemon viminalis)
-"Hibiscus tiliaceus var. tiliaceus"       ==      "Talipariti tiliaceum"# Change to Hibiscus tiliaceus var. tiliaceus
-
-"Panicum maximum" =="Urochloa maxima (Panicum maximum)"# change to Panicum maximum
-  
-  
-  
-  #How many invasive species are in the data set?
+#How many invasive species are in the data set? (138 species)
 int<-sp_list%>%
      filter(Category =="I"| Category =='II')%>%
-     count(.)    
+     dplyr::count(.)    
 
-int #122 invasive species 
 
-#sp_list[sp_list$Category == "NA" & sp_list$native_status == "Native", "NA"] <- "Native"
 
-new<-sp_list %>%
-    mutate(Category = ifelse(native_status == 'Native',"Native",Category))%>%
-    mutate_at(vars(Category), ~replace_na(., "Not Native"))
+new_data<-sp_list %>%
+          mutate(Category = ifelse(native_status == 'Native',"Native",Category))%>% #give native designation
+          mutate_at(vars(Category), ~replace_na(., "Not Native"))%>%#giv non-native designation
+          filter(native_status!="Possibly Native")%>%#remove the designation "Possibly Native ~20 sp
+          dplyr::select(sp, Category)%>%#select only species and category 
+          left_join( .,full_sp, by = "sp")#join to long format community data
     
-    mutate(Category = ifelse(native_status == 'Not Native' & Category!="I"| Category !='II',"Not Native",Category))
-  
-str(new)
-
-sp_list %>% mutate(Category = ifelse(native_status == "Native", "Native", Category))
-
-sp_list[sp_list$native_status=="Native" ,] <- 3
-
-
-#filter taxa list keeping targeted rodents with species or subspecies designations (Note that T&E species do not have sp designations)
-tax_reduced2<-tax%>%
-  filter(order == 'Rodentia',taxonProtocolCategory == 'target', taxonRank%in% c('species','subspecies'))#keeping target, rodents with species designation
-
-
-###WHAT TO DO WITH SUBSPECIES? HERE WE KEEP THEM
-
-
-#make tibbles
-mam_dat<-lapply(neondata, as_tibble)
-head(mam_dat)
-
-#variable descriptions from NEON, i.e. metadata
-#vars<-mam_dat$variables_10072
-
-#this is the tibble with the itv data 
-itv_mammal_data<-mam_dat$mam_pertrapnight
-colnames(itv_mammal_data)
-
-####clean data#####
-
-mammal_dataT <- itv_mammal_data%>%
-  filter(taxonID%in%tax_reduced2$taxonID)%>% #take taxa from neon taxa filtered list #isa: is tax_reduced2 and not tax_reduced
-  mutate(year = year(collectDate), logweight=log(weight))%>% #CREATE YEAR COLUMN and log weight column
-  filter(lifeStage=="adult")%>% #Keep only adults
-  group_by(tagID) %>% #group individuals by tag (i.e., recaptures) 
-  filter(collectDate==min(collectDate))%>% #for recaptures, take the earliest record
-  ungroup()%>%
-  mutate(tax_Site = paste(taxonID, siteID, sep = "_")) #make a unique species-by-site identifier called "tax_Site"
-
-mammal_dataT <- mammal_dataT %>%
-  filter(!is.na(logweight))
+ 
