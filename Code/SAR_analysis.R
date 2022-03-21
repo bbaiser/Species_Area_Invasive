af421c15700d39dbd9a230d6bb388acc50280d33
bbaiser <benj.baiser@gmail.com>
2022-03-21 15:14:43 -0400

    added break out of mainland vs. keys

--- Code/SAR_analysis.R
+++ Code/SAR_analysis.R
@@ -17,11 +17,28 @@ steve_data<-read.csv("Data/park_issues..SWWcomments.csv")%>%#verification of sit
             filter(Keep.=="no")%>%
             dplyr::select(park_name)
 
-reduced_long_dat<-long_dat[!long_dat$park_name %in% steve_data$park_name,]  
-reduced_park_data<-park_data[!park_data$park_name %in% steve_data$park_name,]  
+#remove parks that have insufficient data according to steve from the long_data
+reduced_long_dat<-long_dat[!long_dat$park_name %in% steve_data$park_name,] 
 
-rich<-reduced_long_dat %>%
+#remove parks that have insufficient data according to steve from the long_data
+reduced_park_data<-park_data[!park_data$park_name %in% steve_data$park_name,] 
+
+
+####to separate mainland and keys data
+mainland_park_data<-reduced_park_data%>%
+          filter(Keys=="Mainland")
+
+mainland_long_dat<-reduced_long_dat[reduced_long_dat$park_name %in% mainland_park_data$park_name,]
+
+rich<-mainland_long_dat %>%
       count(CatagoryII,park_name)%>%#get species richness for each provenance by park combo
+      left_join( .,mainland_park_data, by = "park_name")%>%#join with park info (i.e., area, etc)
+      mutate_at(vars(CatagoryII), as.factor)#make catagoryII a factor
+
+
+#not removing keys data
+rich<-reduced_long_dat %>%
+     count(CatagoryII,park_name)%>%#get species richness for each provenance by park combo
       left_join( .,reduced_park_data, by = "park_name")%>%#join with park info (i.e., area, etc)
       mutate_at(vars(CatagoryII), as.factor)#make catagoryII a factor
 
@@ -48,23 +65,30 @@ Exotic<-rich%>%
 
 #test for different z-values
 ancova_model <- aov(log(n) ~ log(size)+CatagoryII, data = rich)
+
 Anova(ancova_model, type="III")
 
 #pairwise posthoc
 postHocs <- glht(ancova_model, linfct = mcp(CatagoryII = "Tukey"))
-
-TukeyHSD(ancova_model, conf.level=.95)
 summary(postHocs)
+plot(postHocs)
+#linear models and plots
 
-#linear models
-
+#native
 summary(lm(log(n)~log(size), data=native))
+plot(log(n)~log(size), data=native)  
 
+#non_native
 summary(lm(log(n)~log(size), data=non_native))
+plot(log(n)~log(size), data=non_native)  
+
+#invasive
 summary(lm(log(n)~log(size), data=Invasive))
+plot(log(n)~log(size), data=Invasive) 
+
+#exotic
 summary(lm(log(n)~log(size), data=Exotic))
-plot(log(n)~log(size), data=Invasive)  
+plot(log(n)~log(size), data=Exotic) 
 
-plot(park_data$sp_num~log(park_data$size))
 
-write.csv(rich, "data/park_rich.csv")
+#write.csv(rich, "data/park_rich.csv")
