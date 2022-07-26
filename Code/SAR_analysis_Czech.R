c75ba27a41a2b6b3d846bf784e17c77305e337eb
bbaiser <benj.baiser@gmail.com>
2022-07-26 15:00:33 -0400

    rough analysis complete for together, arch and neo

--- Code/SAR_analysis_Czech.R
+++ Code/SAR_analysis_Czech.R
@@ -1,4 +1,4 @@
-####Run SAR analysis#######
+###Run SAR analysis###
 library(sf)
 library(lubridate)
 library(plyr)
@@ -9,139 +9,100 @@ library(ggplot2)
 library(rstatix)
 library(emmeans)
 library(lme4)
-install.packages("lmerTest")
 library(lmerTest)
 
+####call in and clean data####
 #import long format data from "pre_analysis.R" file
 long_dat<-read.csv("Data/cleaned_czech_data.csv", row=1)
 
+#import park data
 park_data<-read.csv("Data/Czech_Republic_Parks.csv")
-colnames(park_data)
 
 
-#get species richness for native, exotic, invasive (not including origin)
-
+#get species richness for native, exotic, invasive not including age (i.e., archeophyte, neophyte)
 rich<-long_dat %>%
       count(Invasion.statusII,ID)%>%#get species richness for each provenance by park combo
       left_join( .,park_data, by = "ID")%>%#join with park info (i.e., area, etc)
       mutate_at(vars(Invasion.statusII), as.factor)%>%#make category a factor
       mutate(log_area = log(Area..ha.))
 
-#get species richness for native, exotic, invasive including origin
+#get species richness for native, exotic, invasive including age (i.e., archeophyte, neophyte)
 
-rich_org<-long_dat %>%
+rich_age<-long_dat %>%
       count(Invasion.statusIII,ID)%>%#get species richness for each provenance by park combo
       left_join( .,park_data, by = "ID")%>%#join with park info (i.e., area, etc)
       mutate_at(vars(Invasion.statusIII), as.factor)%>%#make category a factor
       mutate(log_area = log(Area..ha.))
 
-#####################
+###
 #FOR EXOTIC RICHNESS (combine invasive and non-native)
 #rich_exotic<-reduced_long_dat %>%
  #            count(CatagoryIII,park_name)%>%#get species richness for each provenance by park combo
   #           left_join( .,reduced_park_data, by = "park_name")%>%#join with park info (i.e., area, etc)
    #          mutate_at(vars(CatagoryIII), as.factor)%>%#make catagoryII a factor
     #         mutate(log_area = log(size))
-######################################
-#find sites without all three  
-missing<-as.data.frame(sort(table(rich$ID)))%>%
-        filter(Freq<3)
-missing
+###
 
 
+####analysis not considering age####
+
+dat<-rich# not considering age
 
 #test for different z-values
-ancova_model <- aov(log(n) ~ log_area * Invasion.statusIII, data = rich_org)
-
+ancova_model <- aov(log(n) ~ log_area * Invasion.statusII, data = dat)
 Anova(ancova_model, type="III")
 
 #pairwise posthoc
-z<-emtrends(ancova_model,"Invasion.statusIII", var = "log_area")
+z<-emtrends(ancova_model,"Invasion.statusII", var = "log_area")
 pairs(z)
+
 
 ###with random effect for site
-mod<-lmerTest::lmer(log(n) ~ log_area * Invasion.statusIII+(1|ID), data = rich_org)
+mod<-lmerTest::lmer(log(n) ~ log_area * Invasion.statusII+(1|ID), data = dat)
 summary(mod)
+Anova(mod)
 
 #pairwise posthoc
-z<-emtrends(mod,"Invasion.statusIII", var = "log_area")
+z<-emtrends(mod,"Invasion.statusII", var = "log_area")
 pairs(z)
 
 
-
-
-#subset each type of species for models and plots from rich_org (i.e., not considering origin)
-native2<-rich_org%>%
-         filter(Invasion.statusIII=="native native")
-
-non_native_arc<-rich_org%>%
-                filter(Invasion.statusIII=="Exotic archaeophyte")
-
-Invasive_arc<-rich_org%>%
-              filter(Invasion.statusIII=="invasive archaeophyte")
-
-non_native_neo<-rich_org%>%
-                filter(Invasion.statusIII=="Exotic neophyte")
-
-Invasive_neo<-rich_org%>%
-              filter(Invasion.statusIII=="invasive neophyte")
-
-#linear models and plots
-
-#native
-summary(lm(log(n)~log_area, data=native2))
-plot(log(n)~log_area, data=native2) 
-
-#non_native_arc
-summary(lm(log(n)~log_area, data=non_native_arc))
-plot(log(n)~log_area, data=non_native_arc)  
-
-#invasive_arc
-summary(lm(log(n)~log_area, data=Invasive_arc))
-plot(log(n)~log_area, data=Invasive_arc) 
-
-#non_native_neo
-summary(lm(log(n)~log_area, data=non_native_neo))
-plot(log(n)~log_area, data=non_native_neo)  
-
-#invasive_neo
-summary(lm(log(n)~log_area, data=Invasive_neo))
-plot(log(n)~log_area, data=Invasive_neo) 
-
-
-#subset each type of species for models and plots from rich (i.e., not considering origin)
-native<-rich%>%
+###subset each type of species for models and plots from rich (i.e., not considering origin)
+native<-dat%>%
         filter(Invasion.statusII=="native")
 
-non_native<-rich%>%
+non_native<-dat%>%
             filter(Invasion.statusII=="Exotic")
 
-Invasive<-rich%>%
+Invasive<-dat%>%
           filter(Invasion.statusII=="invasive")
 
 
-#linear models and plots
+
+#######linear models and plots
 
 #native
 summary(lm(log(n)~log_area, data=native))
 plot(log(n)~log_area, data=native)  
-
-s=79*600000^0.12836
-log(s)
+abline(lm(log(n)~log_area, data=native))
 
 #non_native
 summary(lm(log(n)~log_area, data=non_native))
 plot(log(n)~log_area, data=non_native)  
+abline(lm(log(n)~log_area, data=non_native))
 
 #invasive
 summary(lm(log(n)~log_area, data=Invasive))
 plot(log(n)~log_area, data=Invasive) 
+abline(lm(log(n)~log_area, data=Invasive))
 
 #exotic
-summary(lm(log(n)~log_area, data=Exotic))
-plot(log(n)~log_area, data=Exotic) 
+#summary(lm(log(n)~log_area, data=Exotic))
+#plot(log(n)~log_area, data=Exotic) 
 
-
+#to calculate species richness from sar equation
+s=79*600000^0.12836
+log(s)
 
 #Plot all three lines ggplot
 ggplot() +
@@ -150,10 +111,90 @@ ggplot() +
   geom_smooth(aes(x = log_area, y = log(n)), data = non_native, 
               method = "lm", se = FALSE, color = "blue") + 
   geom_smooth(aes(x = log_area, y = log(n)), data = Invasive, 
+              method = "lm", se = FALSE, color = "purple")
+
+#####################################################################
+
+###data inputs for all analyses below
+dat<-rich_age#considering age
+
+#test for different z-values
+ancova_model <- aov(log(n) ~ log_area * Invasion.statusIII, data = dat)
+Anova(ancova_model, type="III")
+
+#pairwise posthoc
+z<-emtrends(ancova_model,"Invasion.statusIII", var = "log_area")
+pairs(z)
+
+
+###with random effect for site
+mod<-lmerTest::lmer(log(n) ~ log_area * Invasion.statusIII+(1|ID), data = dat)
+summary(mod)
+Anova(mod)
+
+#pairwise posthoc
+z<-emtrends(mod,"Invasion.statusIII", var = "log_area")
+pairs(z)
+
+#subset each type of species for models and plots from rich_org (i.e.,  considering age)
+native2<-dat%>%
+         filter(Invasion.statusIII=="native native")
+
+non_native_arc<-dat%>%
+                filter(Invasion.statusIII=="Exotic archaeophyte")
+
+Invasive_arc<-dat%>%
+              filter(Invasion.statusIII=="invasive archaeophyte")
+
+non_native_neo<-dat%>%
+                filter(Invasion.statusIII=="Exotic neophyte")
+
+Invasive_neo<-dat%>%
+              filter(Invasion.statusIII=="invasive neophyte")
+
+#linear models and plots
+#native
+summary(lm(log(n)~log_area, data=native2))
+plot(log(n)~log_area, data=native2) 
+abline(lm(log(n)~log_area, data=native2))
+
+#non_native_arc
+summary(lm(log(n)~log_area, data=non_native_arc))
+plot(log(n)~log_area, data=non_native_arc)  
+abline(lm(log(n)~log_area, data=non_native_arc))
+
+#invasive_arc
+summary(lm(log(n)~log_area, data=Invasive_arc))
+plot(log(n)~log_area, data=Invasive_arc) 
+abline(lm(log(n)~log_area, data=Invasive_arc))
+
+#non_native_neo
+summary(lm(log(n)~log_area, data=non_native_neo))
+plot(log(n)~log_area, data=non_native_neo)  
+abline(lm(log(n)~log_area, data=non_native_neo))
+
+#invasive_neo
+summary(lm(log(n)~log_area, data=Invasive_neo))
+plot(log(n)~log_area, data=Invasive_neo) 
+abline(lm(log(n)~log_area, data=Invasive_neo)
+
+
+#Plot all three lines ggplot
+#arc
+ggplot() +
+  geom_smooth(aes(x = log_area, y = log(n)), data = native2, 
+              method = "lm", se = FALSE, color = "green") + 
+  geom_smooth(aes(x = log_area, y = log(n)), data = non_native_arc, 
+              method = "lm", se = FALSE, color = "blue") + 
+  geom_smooth(aes(x = log_area, y = log(n)), data = Invasive_arc, 
+              method = "lm", se = FALSE, color = "purple") 
+#neo
+ggplot() +
+  geom_smooth(aes(x = log_area, y = log(n)), data = native2, 
+              method = "lm", se = FALSE, color = "green") + 
+  geom_smooth(aes(x = log_area, y = log(n)), data = non_native_neo, 
+              method = "lm", se = FALSE, color = "blue") + 
+  geom_smooth(aes(x = log_area, y = log(n)), data = Invasive_neo, 
               method = "lm", se = FALSE, color = "purple") 
 
 
-
- # geom_point(aes(x = year, y = slr), data = brest1, color = "red") + 
-  #geom_point(aes(x = year, y = slr), data = brest2, color = "blue")
-#write.csv(rich, "data/park_rich.csv")
