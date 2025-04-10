8b28b0dd1c95e3a34e36ce991b34444e91606ad0
Baiser <bbaiser@ufl.edu>
2025-04-10 10:20:38 -0400

    cleaned code and made plot for sars

--- Code/SAR_analysis_FISF_Spring_25.R
+++ Code/SAR_analysis_FISF_Spring_25.R
@@ -9,79 +9,59 @@ library(ggplot2)
 library(rstatix)
 library(emmeans)
 
-#import long fromat data from "pre_analysis.R" file
+#import long format data from "pre_analysis.R" file
+
+#species data
 long_dat<-read.csv("Data/cleaned_data.csv")
 
+#park data
 park_data<-read.csv("Data/SFL_meta_data_cleaned.csv")%>%
            rename(size = size..acres.)
   
-
+#expert information about sites with insufficient data
 steve_data<-read.csv("Data/park_issues..SWWcomments.csv")%>%#verification of sites with data i
             filter(Keep.=="no")%>%
             dplyr::select(park_name)
 
-#remove parks that have insufficient data according to steve from the long_data
+#remove parks that have insufficient data from the long_data
 reduced_long_dat<-long_dat[!long_dat$park_name %in% steve_data$park_name,] 
 
 
-#remove parks that have insufficient data according to steve from the park_data
+#remove parks that have insufficient data from the park_data
 reduced_park_data<-park_data[!park_data$park_name %in% steve_data$park_name,] 
 
 
-####to separate mainland and keys data
-#mainland_park_data<-reduced_park_data%>%
-          filter(Keys=="Keys")
-
-#mainland_long_dat<-reduced_long_dat[reduced_long_dat$park_name %in% mainland_park_data$park_name,]
-
-#rich<-mainland_long_dat %>%
-      count(CatagoryII,park_name)%>%#get species richness for each provenance by park combo
-      left_join( .,mainland_park_data, by = "park_name")%>%#join with park info (i.e., area, etc)
-      mutate_at(vars(CatagoryII), as.factor)%>%#make category a factor
-      mutate(log_area = log(size/247.1))
-
-#not removing keys data
+#Obtain richness for native/invasive/nonnative for each park and join park data
 rich<-reduced_long_dat %>%
       count(CatagoryII,park_name)%>%#get species richness for each provenance by park combo
       left_join( .,reduced_park_data, by = "park_name")%>%#join with park info (i.e., area, etc)
       mutate_at(vars(CatagoryII), as.factor)%>%#make catagoryII a factor
-      mutate(log_area = log(size/247.1))
+      mutate(log_area = log(size/247.1))#convert acre to km2
 
-#FOR EXOTIC RICHNESS
+#same as above but lumping non-native and invasive into 
 rich_exotic<-reduced_long_dat %>%
              count(CatagoryIII,park_name)%>%#get species richness for each provenance by park combo
              left_join( .,reduced_park_data, by = "park_name")%>%#join with park info (i.e., area, etc)
              mutate_at(vars(CatagoryIII), as.factor)%>%#make catagoryII a factor
              mutate(log_area = log(size))
 
-#find sites without all three  
+#make sure all sites have native/non-native/invasive species
 missing<-as.data.frame(sort(table(rich$park_name)))%>%
         filter(Freq<3)
 missing
 
 
 
-#test for different z-values
-ancova_model <- aov(log(n) ~ log_area * CatagoryII, data = rich)
-
-Anova(ancova_model, type="III")
-
-
-
-#pairwise posthoc
-z<-emtrends(ancova_model,"CatagoryII", var = "log_area")
-pairs(z)
-
-###with random effect for site
+###test for differnces in z-values with random effect for site
 mod<-lmerTest::lmer(log(n) ~ log_area * CatagoryII+ (1|park_name), data = rich)
 summary(mod)
 Anova(mod)
 
-#pairwise posthoc
+#pairwise post-hoc test
 z<-emtrends(mod,"CatagoryII", var = "log_area")
 pairs(z)
 
-#subset each type of species for models and plots
+#subset each group of species (native/non-native/invasive/exotic) for models and plots
 native<-rich%>%
   filter(CatagoryII=="Native")
 
@@ -96,12 +76,9 @@ Exotic<-rich%>%
 
 
 #linear models and plots
-
 #native
 summary(lm(log(n)~log(size), data=native))
 plot(log(n)~log(size), data=native)  
-s=79*600000^0.12836
-log(s)
 
 #non_native
 summary(lm(log(n)~log(size), data=non_native))
@@ -117,17 +94,19 @@ plot(log(n)~log(size), data=Exotic)
 
 
 
-#Plot all three lines ggplot
-ggplot() +
-  geom_smooth(aes(x = log(size), y = log(n)), data = native, 
-              method = "lm", se = FALSE, color = "green") + 
-  geom_smooth(aes(x = log(size), y = log(n)), data = non_native, 
-              method = "lm", se = FALSE, color = "blue") + 
-  geom_smooth(aes(x = log(size), y = log(n)), data = Invasive, 
-              method = "lm", se = FALSE, color = "purple") 
+#Plot all three (native) lines ggplot
 
-0.05868 +0.02575
+SAR_fisf_plot <- ggplot() +
+                labs(y = "log (Species Richness)", x = expression(log(Area)~km^2)) +
+                geom_smooth(aes(x = log(size), y = log(n)), data = native, 
+                            method = "lm", se = FALSE, color = "green") + 
+                geom_smooth(aes(x = log(size), y = log(n)), data = non_native, 
+                            method = "lm", se = FALSE, color = "blue") + 
+                geom_smooth(aes(x = log(size), y = log(n)), data = Invasive, 
+                            method = "lm", se = FALSE, color = "purple")
 
- # geom_point(aes(x = year, y = slr), data = brest1, color = "red") + 
-  #geom_point(aes(x = year, y = slr), data = brest2, color = "blue")
-#write.csv(rich, "data/park_rich.csv")
+
+
+
+
+
